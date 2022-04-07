9de566e0d435d0a013b7e4e7adedb5398cb9da4c
Giovani Rodrigues <giovani.srodrigues@hotmail.com>
2022-04-07 15:45:42 -0300

    tratativa de erro inputs

--- app/src/main/java/com/example/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/example/gastoviagem/MainActivity.kt
@@ -1,11 +1,55 @@
 package com.example.gastoviagem
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.gastoviagem.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //inflate para ter acesso aos elementos do xml
+        setContentView(binding.root)
+
+        binding.buttonCalculate.setOnClickListener(this)
     }
+
+    override fun onClick(view: View?) {
+        if (view != null) {
+            if (view.id == R.id.button_calculate) {
+                calculate()
+            }
+        }
+    }
+
+    private fun isValid(): Boolean {
+        return (binding.editDistance.text.toString() != ""
+                && binding.editPrice.text.toString() != ""
+                && binding.editAutonomy.text.toString() != ""
+                && binding.editAutonomy.text.toString().toFloat() != 0f)
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun calculate() {
+
+        if (isValid()) {
+            val distance = binding.editDistance.text.toString().toFloat()
+            val price = binding.editPrice.text.toString().toFloat()
+            val autonomy = binding.editAutonomy.text.toString().toFloat()
+
+            val totalValue = (distance * price) / autonomy
+
+            binding.textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+        } else {
+            Toast.makeText(this, R.string.validation_all_fields, Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
